6ea87c1250699da87eb0735bb3dc78db7ecfc348
Philipp Seibt <seibt@pdir.de>
2020-07-15 16:33:08 +0200

    style textarea, radio buttons and checkbox

--- src/Module/ConvertThemeSetup.php
+++ src/Module/ConvertThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\ConvertThemeBundle\Module;
 
 class ConvertThemeSetup extends \BackendModule
 {
-    const VERSION = '1.3.0';
+    const VERSION = '1.3.1';
 
     protected $strTemplate = 'be_converttheme_setup';
 
