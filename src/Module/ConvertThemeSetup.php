108c8e359651aefae45f4a461a5c92da526fc61d
Philipp Seibt <seibt@pdir.de>
2020-08-20 10:48:40 +0200

    add sql files for contao 4.10

--- src/Module/ConvertThemeSetup.php
+++ src/Module/ConvertThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\ConvertThemeBundle\Module;
 
 class ConvertThemeSetup extends \BackendModule
 {
-    const VERSION = '1.3.1';
+    const VERSION = '1.3.2';
 
     protected $strTemplate = 'be_converttheme_setup';
 
