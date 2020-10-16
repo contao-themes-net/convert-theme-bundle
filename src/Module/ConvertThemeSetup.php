0376e193a981a7086449f9fbbb23a9916e88860a
Philipp Seibt <seibt@pdir.de>
2020-10-16 13:31:34 +0200

    add button css for form

--- src/Module/ConvertThemeSetup.php
+++ src/Module/ConvertThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\ConvertThemeBundle\Module;
 
 class ConvertThemeSetup extends \BackendModule
 {
-    const VERSION = '1.3.3';
+    const VERSION = '1.3.4';
 
     protected $strTemplate = 'be_converttheme_setup';
 
